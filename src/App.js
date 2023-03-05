237300d280a0ceff7dfa9b544774fe7d41b8e41c
stark-eth <1319114370@qq.com>
2023-03-05 16:25:55 +0800

    fix typo error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
-import Chatbot from './Components/chatbot/Chatbot'
+import Chatbot from './components/chatbot/Chatbot'
 import { Container } from 'react-bootstrap'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Footer from './Components/Footer'
-import Header from './Components/Header'
-import Faq from './Components/Faq'
-import Help from './Components/Help'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Faq from './components/Faq'
+import Help from './components/Help'
 import './App.css'
-import Slider from './Components/Slider'
+import Slider from './components/Slider'
 
 const App = () => {
   return (
