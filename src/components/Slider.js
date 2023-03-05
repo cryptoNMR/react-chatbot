5d3defff61b3c208d489b9995d16e39364d305fc
stark-eth <1319114370@qq.com>
2023-03-05 16:47:20 +0800

    eslint code

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,28 +6,19 @@ export default function Slider() {
   return (
     <Carousel pause="hover" className="bg-dark">
       <Carousel.Item>
-        <img
-          src="https://github.com/Akash52/react-chatbot/blob/master/public/images/first.jpg?raw=true"
-          fluid
-        />
+        <img src="/images/first.jpg" alt="" fluid />
         <Carousel.Caption className="carousel-caption">
           <h2>Welcome to RMC chatbot</h2>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          src="https://github.com/Akash52/react-chatbot/blob/master/public/images/second.jpg?raw=true"
-          fluid
-        />
+        <img src="/images/second.jpg" alt="" fluid />
         <Carousel.Caption className="carousel-caption">
           <h2>Lets Explore chatbot</h2>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          src="https://raw.githubusercontent.com/Akash52/react-chatbot/master/public/images/third.jpg"
-          fluid
-        />
+        <img src="/images/third.jpg" alt="" fluid />
         <Carousel.Caption className="carousel-caption">
           <h2>Have Great Day !</h2>
         </Carousel.Caption>
